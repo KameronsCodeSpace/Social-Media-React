c3d6e45ce09a30ffbf1e58c67daca85397e75c65
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-01-05 15:22:05 -0500

    A few touch ups on login page

--- front-end/src/Components/Nav.jsx
+++ front-end/src/Components/Nav.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+    const navStyle = {
+        color: 'white'
+    }
+
     return (
 <nav>
     <h3>Logo</h3>
     <ul className="nav-links">
-        <Link to='/Feed'><li>Feed</li></Link>
-        <Link to='/Albums'><li>Albums</li></Link>
-        <Link to='/Workspace'><li>Workspace</li></Link>
-        <Link to='/'><li>Sign Out</li></Link>
+        <Link style={navStyle} to='/Feed'><li>Feed</li></Link>
+        <Link style={navStyle} to='/Albums'><li>Albums</li></Link>
+        <Link style={navStyle} to='/Workspace'><li>Workspace</li></Link>
+        <Link style={navStyle} to='/'><li>Sign Out</li></Link>
         
     </ul>
 </nav>
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -4,8 +4,12 @@ import './App.css';
 function Login() {
     return (
         <div>
-            <h1>Login Page</h1>
-            <input placeholder='Enter Email'></input>
+            <h1>Collage Entourage</h1>
+            <p>A Combination Of Creation</p>
+            <br />
+            <br />
+            <img src='https://media.giphy.com/media/JWGjPl2SpXUFW/giphy.gif'></img>
+            <p>Username</p><input placeholder='Email'></input>
             <br />
             <br />
             <a href="/Feed">Log In</a>
