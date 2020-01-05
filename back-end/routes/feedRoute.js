59ca196e427fd575ab624bde77c8189c6098d0fa
Sharrar Khan <SharrarKhan728@gmail.com>
2020-01-05 17:48:13 -0500

    Server works now

--- back-end/routes/feedRoute.js
+++ back-end/routes/feedRoute.js
@@ -2,12 +2,14 @@
 // who was the poster
 
 const express = require('express');
-const db = require('../pgPromise');
+const pgp = require('pg-promise')();
+const connectionString = "postgres://localhost:5432/seed"; //url where psql is running
+const db = pgp(connectionString); //connected db instance
 
 const Router = express.Router();
 
 // GET ALL POSTS
-router.get("/", async (req, res) => {
+Router.get("/", async (req, res) => {
     try {
         let posts = await db.any(`SELECT * FROM posts`);
         res.json({
@@ -25,3 +27,5 @@ router.get("/", async (req, res) => {
         });
     }
 });
+
+module.exports = Router;
