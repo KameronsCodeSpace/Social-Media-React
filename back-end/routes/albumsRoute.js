5c78cd524d97c7d683d57924ffce3bcb5542707a
Sharrar Khan <SharrarKhan728@gmail.com>
2020-01-05 15:24:26 -0500

    Made db and working on routes

---
+++ back-end/routes/albumsRoute.js
@@ -0,0 +1,67 @@
+// GET /albums/:id - Get all albums that belong to a user.
+// POST /albums/:id - Create new empty album for user.
+
+const express = require('express');
+const db = require('../pgPromise');
+
+const Router = express.Router();
+
+// GET ALBUM BY ID
+Router.get('/:id', async (req, res) => {
+    const userId = req.params.user_id;
+
+    try {
+        const query = 'SELECT * FROM albums WHERE image_id = $1';
+        const data = await db.any(query, [userId]);
+        res.json({
+            message: 'Returned all albums',
+            payload: data
+        });
+    } catch (err) {
+        console.log(err);
+        res.send(`Something went wrong, try again later.`);
+    };
+});
+
+// CREATE A ALBUM
+Router.post('/:id', async (req, res) => {
+    const userId = req.params.user_id;
+    try {
+        const query = 'INSERT INTO albums (image_id) VALUES ($1)';
+        await db.any(query, [userId]);
+        res.json({
+            message: `Created a new album for user ${userId}`,
+            success: true 
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            message: `Something went wrong`,
+            success: false
+        });
+    };
+});
+
+// DELETE SINGLE ALBUM
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    const inputQuery = (`DELETE FROM albums WHERE id = $1`);
+
+    try{
+        await db.none(inputQuery, [id]);
+        res.json({
+            "status": "Success",
+            message: "Deleted a album"
+        });
+    } catch (error) {
+        console.log("Error:", error);
+        res.json({
+            "status": "error",
+            message: "Couldn't delete a album",
+            payload: null
+        });
+    }
+});
+
+
+module.exports = Router;
