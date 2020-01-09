534d8a12f7e0b948cd76047351d1fdbe06f08615
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-01-08 22:26:50 -0500

    Removed jquery and did signup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Landing from './Landing'
 import LoginUser from './Components/LoginUser'
 import SignUp from './Components/SignUp'
 import AuthPages from './AuthPages';
 
+import axios from 'axios'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Landing} />
-          <Route path="/LoginUser" exact component={LoginUser} />
-          <Route path="/SignUp" exact component={SignUp} />
-          <Route path="/" component={AuthPages} />
-        </Switch>
-      </div>
-    </Router>
+import { Switch, Route } from 'react-router-dom';
 
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.initialState = {
+      currentUser: '',
+      email: '',
+      password: ''
+    }
+    this.state = this.initialState
+  }
+
+  renderLoginComponent = () => {
+    return <LoginUser loginUser={this.loginUser} />
+  }
+
+  renderSignupComponent = () => {
+    return <SignUp
+      signUpUser={this.signUpUser}
+      handleEmail={this.handleEmail}
+      handlePassword={this.handlePassword}
+      email={this.state.email}
+      password={this.state.password} />
+  }
+
+  handleEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handlePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+
+  }
+
+  signUpUser = async (event) => {
+    event.preventDefault();
+    try {
+      let response = await axios.post(`http://localhost:3001/auth/signup`,
+        {
+          email: this.state.email,
+          password: this.state.password
+        });
+      this.setState({
+        currentUser: response.data.data,
+      })
+    } catch (error) {
+      console.error(error)
+
+      // const $errorMessage = $('#errorMessage');
+      // $errorMessage.text(error.responseJSON.message);
+      // $errorMessage.show();
+    }
+    console.log('User', this.user)
+  }
+
+
+
+  //   signup = () => {
+  //     console.log('User', this.user)
+  //     const API_URL = this.getHostURL();
+  //     const AUTH_URL = `${API_URL}/auth`;
+
+  //     return get.post(`${AUTH_URL}/signup`, user)
+  // }
+
+  // getHostURL = () => {
+  //   if (window.location.host.indexOf('localhost') !== -1) {
+  //       return 'http://localhost:3001';
+  //   } else {
+  //       return 'https://collage-entourage.com';
+  //   }
+  // }
+
+  render() {
+    return (
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/LoginUser" exact render={this.renderLoginComponent} />
+            <Route path="/SignUp" exact render={this.renderSignupComponent} />
+            <Route path="/" component={AuthPages} />
+          </Switch>
+        </div>
+
+    );
+  }
 }
 
 export default App;
--- front-end/src/Components/SignUp.jsx
+++ front-end/src/Components/SignUp.jsx
@@ -1,139 +1,6 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React from 'react';
 
-class SignUp extends Component {
-
-    componentDidMount() {
-        // Query doesn't redefine the this pointer, but that's how JavaScript 
-        // functions work in general. Store a reference to the this pointer under 
-        // a different name, and use that.
-        const self = this;
-
-        $.ajaxSetup({
-            crossDomain: true,
-            xhrFields: {
-                withCredentials: true
-            },
-
-        });
-
-        $(document).ready(function () {
-            // get user id from url query
-            const params = self.parseQuery(window.location.search);
-            // make a request to the server for the user information
-            self.getUserInfo(params.id)
-                .then(this.addUserInfoToPage)
-                .then(this.getImages)
-                .then(this.addImages)
-                .catch(this.noUserFound);
-            // show user information
-            // make a request to server for the stickers for the user with that id
-            // show user stickers
-        });
-
-        //sign up jquery
-        $(() => {
-            console.log("Hello from jquery")
-            $('form').submit((event) => {
-                event.preventDefault();
-
-                console.log("submitted")
-                const email = $('#email').val();
-                const password = $('#password').val();
-
-                const user = {
-                    email,
-                    password
-                }
-
-                console.log('User', user)
-
-                self.signup(user)
-                    .then(result => {
-                        console.log(result)
-                        window.location = `user.html?id=${result.id}`
-                    }).catch(error => {
-                        console.error(error)
-
-                        const $errorMessage = $('#errorMessage');
-                        $errorMessage.text(error.responseJSON.message);
-                        $errorMessage.show();
-                    })
-
-            })
-        })
-    }
-
-    login = (user) => {
-        const API_URL = this.getHostURL();
-        const AUTH_URL = `${API_URL}/auth`;
-
-        return $.post(`${AUTH_URL}/login`, user)
-    }
-
-    signup = (user) => {
-        console.log('User', user)
-        const API_URL = this.getHostURL();
-        const AUTH_URL = `${API_URL}/auth`;
-
-        return $.post(`${AUTH_URL}/signup`, user)
-    }
-
-    parseQuery = (query) => {
-        return query.substr(1).split('&').reduce((params, keyValue) => {
-            const parts = keyValue.split('=');
-            params[parts[0]] = parts[1];
-            return params
-        }, {});
-    }
-
-    getUserInfo = (id) => {
-        const API_URL = this.getHostURL();
-
-        return $.get(`${API_URL}/users/${id}`)
-    }
-
-    getImages = (id) => {
-        const API_URL = this.getHostURL();
-
-        return $.get(`${API_URL}/users/${id}/images`)
-    }
-
-    addUserInfoToPage = (user) => {
-        // let source = $("#user-template").html();
-        // let template = Handlebars.compile(source);
-        // let context = user;
-        // let html = template(context);
-        // $('.user').html(html);
-        // return user.id;
-    }
-
-    addImages = (images) => {
-        // let source = $("#sticker-template").html();
-        // let template = Handlebars.compile(source);
-        // let context = { images };
-        // yay more variations!! abstraction rocks!
-        // let context = {images: images};
-        // let html = template(context);
-        // $('.images').html(html);
-    }
-
-    getHostURL = () => {
-        if (window.location.host.indexOf('localhost') !== -1) {
-            return 'http://localhost:3001';
-        } else {
-            return 'https://collage-entourage.com';
-        }
-    }
-
-    noUserFound = () => {
-        alert('user not found...')
-    }
-
-    render() {
-        const divStyle = {
-            display: 'none'
-        };
+const SignUp = (props) => {
 
         return (
             <div>
@@ -141,15 +8,15 @@ class SignUp extends Component {
 
                 <main>
                     <section>
-                        <div className="alert alert-danger" role="alert" id="errorMessage" style={divStyle}></div>
-                        <form className="form">
+                        <div className="alert alert-danger" role="alert" id="errorMessage"></div>
+                        <form className="form" onSubmit={props.signUpUser}>
                             <div className="form-group">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" id="email" placeholder="Enter Email" required />
+                                <input type="email" className="form-control" id="email" placeholder="Enter Email" required onChange={props.handleEmail}  value={props.email}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Enter Password" required />
+                                <input type="password" className="form-control" id="password" placeholder="Enter Password" required onChange={props.handlePassword} value={props.password}/>
                             </div>
                             <button typeof="submit" className="btn btn-default btn-success" href="/albums">Create Account</button>
                         </form>
@@ -159,7 +26,133 @@ class SignUp extends Component {
         );
     }
 
-}
-
-
 export default SignUp;
+
+// class SignUp extends Component {
+
+    //     componentDidMount() {
+    //         // Query doesn't redefine the this pointer, but that's how JavaScript 
+    //         // functions work in general. Store a reference to the this pointer under 
+    //         // a different name, and use that.
+    //         const self = this;
+    
+    //         $.ajaxSetup({
+    //             crossDomain: true,
+    //             xhrFields: {
+    //                 withCredentials: true
+    //             },
+    
+    //         });
+    
+    //         $(document).ready(function () {
+    //             // get user id from url query
+    //             const params = self.parseQuery(window.location.search);
+    //             // make a request to the server for the user information
+    //             self.getUserInfo(params.id)
+    //                 .then(this.addUserInfoToPage)
+    //                 .then(this.getImages)
+    //                 .then(this.addImages)
+    //                 .catch(this.noUserFound);
+    //             // show user information
+    //             // make a request to server for the stickers for the user with that id
+    //             // show user stickers
+    //         });
+    
+    //         //sign up jquery
+    //         $(() => {
+    //             console.log("Hello from jquery")
+    //             $('form').submit((event) => {
+    //                 event.preventDefault();
+    
+    //                 console.log("submitted")
+    //                 const email = $('#email').val();
+    //                 const password = $('#password').val();
+    
+    //                 const user = {
+    //                     email,
+    //                     password
+    //                 }
+    
+    //                 console.log('User', user)
+    
+    //                 self.signup(user)
+    //                     .then(result => {
+    //                         console.log(result)
+    //                         window.location = `user.html?id=${result.id}`
+    //                     }).catch(error => {
+    //                         console.error(error)
+    
+    //                         const $errorMessage = $('#errorMessage');
+    //                         $errorMessage.text(error.responseJSON.message);
+    //                         $errorMessage.show();
+    //                     })
+    
+    //             })
+    //         })
+    //     }
+    
+    
+    //     signup = (user) => {
+    //         console.log('User', user)
+    //         const API_URL = this.getHostURL();
+    //         const AUTH_URL = `${API_URL}/auth`;
+    
+    //         return $.post(`${AUTH_URL}/signup`, user)
+    //     }
+    
+    //     parseQuery = (query) => {
+    //         return query.substr(1).split('&').reduce((params, keyValue) => {
+    //             const parts = keyValue.split('=');
+    //             params[parts[0]] = parts[1];
+    //             return params
+    //         }, {});
+    //     }
+    
+    //     getUserInfo = (id) => {
+    //         const API_URL = this.getHostURL();
+    
+    //         return $.get(`${API_URL}/users/${id}`)
+    //     }
+    
+    //     getImages = (id) => {
+    //         const API_URL = this.getHostURL();
+    
+    //         return $.get(`${API_URL}/users/${id}/images`)
+    //     }
+    
+    //     addUserInfoToPage = (user) => {
+    //         // let source = $("#user-template").html();
+    //         // let template = Handlebars.compile(source);
+    //         // let context = user;
+    //         // let html = template(context);
+    //         // $('.user').html(html);
+    //         // return user.id;
+    //     }
+    
+    //     addImages = (images) => {
+    //         // let source = $("#sticker-template").html();
+    //         // let template = Handlebars.compile(source);
+    //         // let context = { images };
+    //         // yay more variations!! abstraction rocks!
+    //         // let context = {images: images};
+    //         // let html = template(context);
+    //         // $('.images').html(html);
+    //     }
+    
+    //     getHostURL = () => {
+    //         if (window.location.host.indexOf('localhost') !== -1) {
+    //             return 'http://localhost:3001';
+    //         } else {
+    //             return 'https://collage-entourage.com';
+    //         }
+    //     }
+    
+    //     noUserFound = () => {
+    //         alert('user not found...')
+    //     }
+    
+    //     render() {
+    //         const divStyle = {
+    //             display: 'none'
+    //         };
+    
