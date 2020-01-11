417bbbe0dff08762c8aed21bcb0164a2f0ca3e5b
Sharrar Khan <SharrarKhan728@gmail.com>
2020-01-11 16:51:30 -0500

    Worked on albums and added tables to the DB

--- front-end/src/Albums.js
+++ front-end/src/Albums.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 
-function Albums() {
-    return (
-        <div>
-            <h1 id="albumsPageHeader">Albums Page</h1>
-            <div id="addAlbumButtonDiv">
-                <button type="button" id="addAlbum" class="btn btn-success btn-circle btn-xl">Add</button> 
+class Albums extends Component {
+    constructor() {
+        super();
+        this.state = {
+            numberOfAlbums: [],
+            albumInputValue: ""
+        }
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+        albumInputValue: event.target.value
+        });
+    }
+
+    addAlbum = async () => {
+        let album_owner;
+        let album_name = this.state.album_name;
+        let response = await axios.post(`http://localhost:3000/albums/`, { album_owner, album_name });
+    }
+
+    render() {
+        const { albumInputValue } = this.state;
+        return (
+            <div>
+                <div>
+                    <h1 id="albumsPageHeader">Albums Page</h1>
+                    <form onSubmit="">
+                        <input 
+                            id="albumNameInput" 
+                            placeholder="Album Name" 
+                            value={albumInputValue}
+                            onChange={this.albumInputValue}
+                        />
+                        <button type="button" id="addAlbumButton" class="btn btn-success btn-circle btn-xl">Add</button> 
+                    </form>
+                </div>
+
+                <div>
+
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Albums;
+
+// state, handlers for input, handle click, form submit
